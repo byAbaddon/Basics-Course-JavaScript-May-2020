c7ab20f83d317994de2b1bebe2c817b7373529b2
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-11-01 23:38:28 +0200

    Update 04. Factorial.js

--- 4.1 For Loop - Exercise/04. Factorial.js
+++ 4.1 For Loop - Exercise/04. Factorial.js
@@ -1,3 +1,12 @@
+function factorial(f) {    //[be black Ninja]//
+  return f == 0 ? 1 : f * factorial(f - 1)
+}
+
+// console.log(factorial('8'))
+
+-----------------------------------------------(2)----------------------------
+
+
 function factorial(n) {
   if (n == 0) {
     return 1
